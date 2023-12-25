c467373c90240e5a2ea80580a422efa77b18f102
anilercan_23 <52035643+jrAnocan@users.noreply.github.com>
2023-12-25 06:10:27 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// function fetchHTMLContent(url, callback) {
+//     fetch(url)
+//         .then(response => response.text())
+//         .then(data => callback(data))
+//         .catch(error => console.error('Error fetching HTML content:', error));
+// }
+
+// // Your HTML file path
+// var htmlFilePathButton = '../incrementButton/index.html';
+// // Get elements with the class name "face face2"
+// var targetElement = document.getElementsByClassName('face face2')[0];
+
+
+
+// // Fetch HTML content from the file
+// fetchHTMLContent(htmlFilePathButton, function(htmlContent) {
+    
+//     var newElement = document.createElement('div');
+//     newElement.className='content';
+//     newElement.innerHTML = htmlContent;
+
+//     // Append the new element to the first child of the target element
+    
+//     targetElement.appendChild(newElement);
+    
+// });
+    
+
